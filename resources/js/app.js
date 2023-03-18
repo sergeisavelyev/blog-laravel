bb4369d2f874d751e0d399f794577fd25311d612
sergeisavelyev <sergeisavelyev11@gmail.com>
2023-03-18 15:59:56 +0300

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import '~admin/plugins/jquery/jquery.min.js';
+import '~admin/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '~admin/js/adminlte.js';
+import '~admin/js/demo.js';
+
+import.meta.glob([
+    '../assets/admin/img/**',
+    '~admin/plugins/fontawesome-free/webfonts/**',
+])
