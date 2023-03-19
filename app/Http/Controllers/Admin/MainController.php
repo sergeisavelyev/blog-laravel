16b78062e912486c60c1639b33d035295d81e717
sergeisavelyev <sergeisavelyev11@gmail.com>
2023-03-19 12:06:59 +0300

    changed models

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
-    { 
+    {
         return view('admin.index');
     }
 }
