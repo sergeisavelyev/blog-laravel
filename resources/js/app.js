224d5339f7b1856719f2df5d46213b07a8833164
sergeisavelyev <sergeisavelyev11@gmail.com>
2023-03-20 15:16:51 +0300

    changed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
-import '~admin/plugins/jquery/jquery.min.js';
+import '~admin/plugins/jquery/jquery.js';
 import '~admin/plugins/bootstrap/js/bootstrap.bundle.min.js';
 import '~admin/js/adminlte.js';
 import '~admin/js/demo.js';
