edba232a8aa6088aaf9d1211fce6483a8ee4d8ca
sergeisavelyev <sergeisavelyev11@gmail.com>
2023-03-25 11:26:55 +0300

    index page, layout, post page added

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import '~admin/plugins/bootstrap/js/bootstrap.bundle.min.js';
 import '~admin/plugins/select2/js/select2.full.js';
 import '~admin/js/adminlte.js';
 import '~admin/js/demo.js';
+import '~admin/plugins/bs-custom-file-input/bs-custom-file-input.js';
 
 import.meta.glob([
     '../assets/admin/img/**',
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', 'PostController@index')->name('home');
+Route::get('/article', 'PostController@show')->name('posts.single');
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
     Route::get('/', 'MainController@index')->name('admin.index');
